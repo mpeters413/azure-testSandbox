d04c9a95cb11379dbcf8d2b782636ab9da0b28dd
Matthew Peters <mpeters413@gmail.com>
2019-04-16 09:30:35 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ module "database" {
   source  = "app.terraform.io/mattpeters/database/azurerm"
   version = "1.1.0"
 
-  db_name = "mattsDB"
+  db_name = "mattsdb"
   location = "eastus"
   sql_admin_username = "matt"
   sql_password = "password"
