afce053dd83142eff04da258b9209ea0251ea2d8
Matthew Peters <mpeters413@gmail.com>
2019-04-15 21:38:10 -0400

    Add files via upload

---
+++ main (1).tf
@@ -0,0 +1,11 @@
+//--------------------------------------------------------------------
+// Modules
+module "vm" {
+  source  = "app.terraform.io/mattpeters/vm/azurerm"
+  version = "0.0.1"
+
+  location = "eastus"
+  name = "mattService"
+  namespace = "mattsNamespace"
+  vm_size = "VM1"
+}
